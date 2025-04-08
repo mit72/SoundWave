76d29b54acbc04f4b8c02e8d68ca6bdcb859cfd9
mitja <mitjafilej431@gmail.com>
2025-04-08 23:06:58 +0200

    changed main class to HelloApplication

--- src/main/java/com/example/final13/HelloApplication.java
+++ src/main/java/com/example/final13/HelloApplication.java
@@ -14,7 +14,6 @@ import java.io.IOException;
 public class HelloApplication extends Application {
 
 
-
     @Override
     public void start(Stage stage) throws IOException {
         FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("hello-view.fxml"));
@@ -35,7 +34,9 @@ public class HelloApplication extends Application {
         stage.show();
     }
 
-    public static void main(String[] args) {
-        launch();
-    }
+
+
+public static void main(String[] args) {
+    launch();
+}
 }
