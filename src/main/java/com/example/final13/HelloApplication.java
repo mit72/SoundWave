b5588fd4896e8f699a8f45b56ab5362dc02700be
mitja <mitjafilej431@gmail.com>
2025-04-13 22:45:09 +0200

    fixed jar launch

--- src/main/java/com/example/final13/HelloApplication.java
+++ src/main/java/com/example/final13/HelloApplication.java
@@ -34,9 +34,7 @@ public class HelloApplication extends Application {
         stage.show();
     }
 
-
-
-public static void main(String[] args) {
-    launch();
-}
+    public static void main(String[] args) {
+        launch();
+    }
 }
---
+++ src/main/java/com/example/final13/Launch.java
@@ -0,0 +1,9 @@
+package com.example.final13;
+
+import javafx.application.Application;
+
+public class Launch {
+    public static void main(String[] args) {
+        Application.launch(HelloApplication.class, args);
+    }
+}
