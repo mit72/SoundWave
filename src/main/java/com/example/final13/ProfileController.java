537f0d5e11ecc905562468df3875c02427b72964
mitja <mitjafilej431@gmail.com>
2025-05-06 21:00:47 +0200

    just visual structure

--- src/main/java/com/example/final13/ProfileController.java
+++ src/main/java/com/example/final13/ProfileController.java
@@ -1,4 +1,19 @@
 package com.example.final13;
 
+import javafx.fxml.FXML;
+import javafx.scene.control.ToggleButton;
+
 public class ProfileController {
+    @FXML
+    private ToggleButton toggleSwitch;
+
+    @FXML
+    private void handleToggle() {
+        if (toggleSwitch.isSelected()) {
+            System.out.println("Switch ON");
+        } else {
+            System.out.println("Switch OFF");
+        }
+    }
+
 }
