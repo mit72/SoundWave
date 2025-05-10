6ca330fe8828094e60a809ca0bf41cab64383050
mitja <mitjafilej431@gmail.com>
2025-05-10 16:41:33 +0200

    UI redesign

--- src/main/java/com/example/final13/ProfileController.java
+++ src/main/java/com/example/final13/ProfileController.java
@@ -4,15 +4,33 @@ import javafx.fxml.FXML;
 import javafx.scene.control.ToggleButton;
 
 public class ProfileController {
-    @FXML
-    private ToggleButton toggleSwitch;
+    @FXML private ToggleButton toggleSwitch;
+    @FXML private ToggleButton togglePrivateSession;
+
+    public void initialize(){
+        toggleSwitch.setText("O");
+        togglePrivateSession.setText("O");
+    }
 
     @FXML
     private void handleToggle() {
         if (toggleSwitch.isSelected()) {
             System.out.println("Switch ON");
+            toggleSwitch.setText("O");
         } else {
             System.out.println("Switch OFF");
+            toggleSwitch.setText("O");
+        }
+    }
+
+    @FXML
+    private void handleTogglePrivateSession() {
+        if (togglePrivateSession.isSelected()) {
+            System.out.println("Switch ON");
+            togglePrivateSession.setText("O");
+        } else {
+            System.out.println("Switch OFF");
+            togglePrivateSession.setText("O");
         }
     }
 
