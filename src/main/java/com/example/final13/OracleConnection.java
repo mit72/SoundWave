ed42ee02e531af8a0d5343ebfde719d22cfa122a
mitja <mitjafilej431@gmail.com>
2025-05-11 02:15:23 +0200

    update for charts

--- src/main/java/com/example/final13/OracleConnection.java
+++ src/main/java/com/example/final13/OracleConnection.java
@@ -1,8 +1,10 @@
 package com.example.final13;
 
 import java.sql.Connection;
+import java.sql.Date;
 import java.sql.DriverManager;
 import java.sql.SQLException;
+import java.time.LocalDate;
 
 public class OracleConnection {
     private static final String URL = "jdbc:oracle:thin:@193.2.139.22:1521:ers";
@@ -27,5 +29,15 @@ public class OracleConnection {
             System.out.println("Ne dela :)");
         }
     }
+
+    public static Date toSqlDate(LocalDate date) {
+        return date != null ? Date.valueOf(date) : null;
+    }
+
+    public static LocalDate toLocalDate(Date date) {
+        return date != null ? date.toLocalDate() : null;
+    }
 }
+
+
 
